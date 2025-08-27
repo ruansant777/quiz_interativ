69aaedb0aa365fef1f2e73f114449162fa6df29c
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-08-27 07:44:15 -0300

    Update pergunta.js

--- js/pergunta.js
+++ js/pergunta.js
@@ -1,1 +1,97 @@
-
+const perguntas = [
+  {
+    enunciado: "fortinite ou free fire?"
+    alternativas: [
+       {
+          texto: "fortinite",
+          afirmacao: [
+            "fortinite é melhor",
+            "boa"
+          ], 
+       },
+       {
+          texto: "free fire",
+          afirmacao: [
+            "cê é bobão",
+            "nada a ve "
+          ], 
+       }, 
+    ]
+  },
+{
+    enunciado: "vilão ou herói?"
+    alternativas: [
+       {
+          texto: "vilão",
+          afirmacao: [
+            "dependendo do vilão até vai",
+            "todo vilão é mal se a história for contada pelo herói..."
+          ], 
+       },
+       {
+          texto: "herói",
+          afirmacao: [
+            "dependendo do herói...",
+            "daora"
+          ], 
+       }, 
+    ]
+  },
+  {
+    enunciado: "homem aranha 1, 2 ou 3?"
+    alternativas: [
+       {
+          texto: "afirmativa 1",
+          afirmacao: [
+            "afirmação 1",
+            "afirmação 2"
+          ], 
+       },
+       {
+          texto: "afirmativa 2",
+          afirmacao: [
+            "afirmação 1",
+            "afirmação 2"
+          ], 
+       }, 
+    ]
+  },
+    {
+    enunciado: "livro ou filme?"
+    alternativas: [
+       {
+          texto: "afirmativa 1",
+          afirmacao: [
+            "afirmação 1",
+            "afirmação 2"
+          ], 
+       },
+       {
+          texto: "afirmativa 2",
+          afirmacao: [
+            "afirmação 1",
+            "afirmação 2"
+          ], 
+       }, 
+    ]
+  },
+      {
+    enunciado: "lavar 20 pratos e 40 talheres ou 5 panelas engorduradas"
+    alternativas: [
+       {
+          texto: "afirmativa 1",
+          afirmacao: [
+            "afirmação 1",
+            "afirmação 2"
+          ], 
+       },
+       {
+          texto: "afirmativa 2",
+          afirmacao: [
+            "afirmação 1",
+            "afirmação 2"
+          ], 
+       }, 
+    ]
+  }
+]
