19641c3c7bb38a19bc2b5b211342fb1bd5c4c06a
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-09-04 12:04:46 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["calabreso", "linguiço", "fernando", "luiso", "reginaldo", "donald",];
 
+function aleatorio(lista){
+  const posicao = Math.floor(Math.random()* listaa.length);
+  return lista[posicao];
+}
+
+const nome = aleatorio(nomes)
